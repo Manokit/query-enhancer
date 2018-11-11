4b2b5aeede31dc85fb169646116fd953770a5991
Unknown <bbargay@gmail.com>
2018-11-11 10:05:07 -0700

    Final Changes

--- enhancedSearch.py
+++ enhancedSearch.py
@@ -10,6 +10,7 @@ from mongoBall import mongoBall
 import sys,os,datetime
 import json
 from bson.objectid import ObjectId
+import math
 
 def usage():
     print __doc__
@@ -19,12 +20,13 @@ def usage():
 def enhancedSearch(searchQueryIn, mdb_collection, dict_field_name, stopwords_set):
     #figure out how this data is being inserted into the DB
 
+
     #tokenize input search
+    searchQueryLen = len(searchQueryIn.split())
     searchQueryTokensList = ta.tokenize(searchQueryIn, stopwords_set)
     searchTokenFrequency = ta.termFrequency(searchQueryTokensList)
     searchRec = (ObjectId('5be8329564212212ed1dd1f0'), searchTokenFrequency)
     invertedSTF = ta.invert(searchRec)
-    print invertedSTF
 
     mdb_captions_query_format = {"caption_token_dict":1}
     mdb_query = mdb_collection.find({}, mdb_captions_query_format)
@@ -32,18 +34,52 @@ def enhancedSearch(searchQueryIn, mdb_collection, dict_field_name, stopwords_set
 
 
     #print captionTokenFrequenciesList[0]
+
+    cosSimScore = {}
+    STFset = set(searchTokenFrequency)
     for rec in captionTokenFrequenciesList:
-        invertedCTF = ta.invert(rec)
-        print invertedCTF
-        #print ta.commonKeys(invertedSTF, invertedCTF)
+        CTFset = set(rec[1])
+        commonTokens = {}
+        for token in STFset.intersection(CTFset):
+            commonTokens.update({token:[searchTokenFrequency[token],rec[1][token]]})
+        if(commonTokens):
+            #print searchQueryLen
+            missingTokens = searchQueryLen - len(commonTokens) * 1.0
+            #print commonTokens
+            #print missingTokens
+            dotProd = 0.0
+            a2=0.0
+            b2=0.0
+            for token in commonTokens:
+                dotProd += commonTokens[token][0] * commonTokens[token][1]
+                a2 += commonTokens[token][0] ** 2
+                b2 += commonTokens[token][1] ** 2
+                #print a2, commonTokens[token][0]
+                #print b2, commonTokens[token][1]
 
-    a=[]
-    for tokens in invertedSTF:
-        a.append(searchTokenFrequency[token])
-        b.append(caption)
+            #print dotProd
+            #print math.sqrt(a2)
+            #print math.sqrt(b2)
+            score = ((dotProd) / (math.sqrt(a2)*math.sqrt(b2)))
+            if(missingTokens != 0):
+                score *= (1.0/missingTokens)
+            #print rec[0], score
+            cosSimScore.update({rec[0] : score})
 
-    ta.cosineSimilarity().cossim()
+        else:
+            cosSimScore.update({rec[0] : 0.0})
 
+    mdb_query = mdb_collection.find({}, {"youtubeURL":1})
+    urlScoreList = {}
+    for rec in mdb_query:
+        urlScoreList.update({rec["youtubeURL"] : cosSimScore[rec['_id']]})
+
+    sortedList = sorted(urlScoreList.items(), key=lambda x: x[1], reverse=True)
+
+    for url in sortedList:
+        print url[0], url[1]
+
+    #print commonTokens
     #print query_text
 
 
